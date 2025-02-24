6b5393ea6ee856b577f3e815e0c71bf1a21c3bca
SRobver <soumyaranjanbehera920@gmail.com>
2025-02-25 01:13:22 +0530

    added the local storage, some css and blastConfetti

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const storedTasks=JSON.parse(localStorage.getItem('tasks'));
+
+if(storedTasks){
+    storedTasks.forEach((task)=>tasks.push(task));
+    updateTaskList();
+    updateStats();
+}
+})
+
 let task=[];
+
+const saveTasks=()=>{
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
 
 const addTask=()=>{
     const taskInput=document.getElementById("taskInput");
@@ -6,26 +20,51 @@ const addTask=()=>{
 
     if(text){
         task.push({text:text,status:false});
+        taskInput.value="";
+        updateTaskList();
+        updateStats();
+        saveTasks();
     }
     console.log(task);
 
-    updateTaskList();
+    
 }
 
 const toggleTaskComplete=(index)=>{
     task[index].completed=!task[index].completed;
     console.log(task);
+    updateStats();
+    saveTasks();
 }
 const deleteTask=(index)=>{
     task.splice(index,1);
     updateTaskList();
+    updateStats();
+    saveTasks();
 }
 const editTask=(index)=>{
  const taskInput=   document.getElementById("taskInput");
  taskInput.value=task[index].text;
  task.splice(index,1);
  updateTaskList();
+ updateStats();
+ saveTasks();
 };
+
+const updateStats=()=>{
+    const completeTasks=tasks.filter(task=>task.completed).length;
+    const totalTasks=tasks.length;
+    const progress=(completeTasks/totalTasks)*100;
+    const progressBar =document.getElementById('progress');
+    progressBar.style.width=`${progress}%`;
+
+    document.getElementById("numbers").innerText=`${completeTasks}/${totalTasks}`;
+
+    if(tasks.length && completeTasks===totalTasks){
+        blastConfetti();
+
+    }
+}
 
 const updateTaskList=function(){
     const taskList=document.getElementById("task-list");
@@ -57,3 +96,47 @@ document.getElementById("newTask").addEventListener("click",function(event){
    addTask();
    document.getElementById("taskInput").value="";
 });
+
+
+
+const blastConfetti=()=>{
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
